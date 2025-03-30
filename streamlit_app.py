f7927955677c951b85276b97c383c10a6da81fda
LeoAguilera <leoaguilerapiedra@gmail.com>
2025-03-30 15:51:24 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 #st.dataframe(data=pd_df, use_container_width=True)
 #st.stop()
 
