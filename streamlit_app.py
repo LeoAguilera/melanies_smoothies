86625fc79e86b62a08bc6b010e020d50ff57b9da
LeoAguilera <leoaguilerapiedra@gmail.com>
2025-03-30 13:59:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+name_on_order+"!", icon="✅")
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.jason())
+st.text(smoothiefroot_response)
